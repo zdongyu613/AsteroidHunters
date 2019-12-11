c956bc2dc4817e0d1132bafd8ca8868fab0d165e
zdongyu <zdongyu@umich.edu>
2019-12-11 00:05:33 -0500

    data collecting complete
    Data collection complete, trying to store things in to database

--- NASASpider.py
+++ NASASpider.py
@@ -8,11 +8,11 @@ NEO_API_KEY = 'jvZwakvScvkB3hk3XIAKmoYcQULwIkpPreD7JnHj'
 DB = os.getcwd() + '\Asteroids_NASA.db'
 
 
-def get_neo_data(start_date, end_date=''):
+def get_neo_data(start_date):
     # date form: yyyy-mm-dd
-    # the default end date is 7 days after start date
-    # First API using, get rough data of asteroids near earth in given date range
+    # First API using, get rough data of asteroids near earth from the given start date to 7 days later
     # data from NASA NeoWs
+    end_date = ''
     url = 'https://api.nasa.gov/neo/rest/v1/feed?start_date={}&end_date={}&api_key={}'.format(
         start_date, end_date, NEO_API_KEY
     )
@@ -25,11 +25,9 @@ def get_neo_data(start_date, end_date=''):
     # category asteroids data by date
     # structure:
     # {
-    #   yyyy-mm-dd:{
-    #     asteroid_name:{
+    #    asteroid_name:{
     #       estimated_min,estimate_max,is_potentially_hazardous
-    #     },
-    #   },
+    #    },
     # }
     # asteroid data includes:
     #   name: name of asteroid in NASA data
@@ -39,42 +37,45 @@ def get_neo_data(start_date, end_date=''):
     #       True for hazardous, False for not hazardous
     ##################################################################
     for date in asteroids_all.items():
-        asteroids_size_data[date[0]] = {}
         for asteroid in date[1]:
             name = asteroid['name']
-            asteroids_size_data[date[0]][name] = {}
-            asteroids_size_data[date[0]][name]['estimated_min'] = asteroid['estimated_diameter']['meters']['estimated_diameter_min']
-            asteroids_size_data[date[0]][name]['estimated_max'] = asteroid['estimated_diameter']['meters']['estimated_diameter_max']
-            asteroids_size_data[date[0]][name]['is_potentially_hazardous'] = asteroid['is_potentially_hazardous_asteroid']
+            asteroids_size_data[name] = {}
+            asteroids_size_data[name]['estimated_min'] = asteroid['estimated_diameter']['meters']['estimated_diameter_min']
+            asteroids_size_data[name]['estimated_max'] = asteroid['estimated_diameter']['meters']['estimated_diameter_max']
+            asteroids_size_data[name]['is_potentially_hazardous'] = asteroid['is_potentially_hazardous_asteroid']
 
     return asteroids_size_data
 
 
-def get_sentry_data():
-    # this function get all asteroids which have potential future Earth impact events.
+def get_sentry_data(impact_p):
+    # this function get asteroids which have potential future Earth impact events.
+    #
+    # impact_p limit data to those with a impact-probability (IP)
+    # greater than or equal to this value
+    # impact_p range: [-10,0]
+    #
     # data from Sentry System API
-    url = 'https://ssd-api.jpl.nasa.gov/sentry.api'
+    ipmin = '1e-{}'.format(impact_p)
+    url = 'https://ssd-api.jpl.nasa.gov/sentry.api?ip-min={}'.format(ipmin)
     raw_data = requests.get(url).text
     json_data = json.loads(raw_data)
     asteroids_data = json_data['data']
 
-    asteroids_size_data = {'all_potential_impact': {}}
+    asteroids_size_data = {}
     #############################################################
     # structure:
     # {
-    #   all_potential_impact:{
-    #       asteroid_name:{
-    #           diameter, designation
-    #       }
-    #   }
+    #    name:{
+    #       diameter, designation
+    #    },
     # }
     # asteroid data includes:
-    #   asteroid_name: asteroid full name
+    #   name: asteroid full name
+    #   designation: use to recognize which asteroid it is, often same as name
     #   diameter: in kilometer
-    #   designation: often same as asteroid's name
     #############################################################
     for asteroid in asteroids_data:
-        asteroids_size_data['all_potential_impact'[asteroid['fullname']]] = {
+        asteroids_size_data[asteroid['fullname']] = {
             'diameter': asteroid['diameter'],
             'designation': asteroid['des']
         }
@@ -82,16 +83,67 @@ def get_sentry_data():
     return asteroids_size_data
 
 
-def store_in_db(dic, db):
+def get_ca_data(start_date):
+    # start_date form: yyyy-mm-dd
+    # Third API using, get rough data of asteroids near earth from the given start date to 60 days later
+    # data from NASA NeoWs
+    url = 'https://ssd-api.jpl.nasa.gov/cad.api?date-min={}&body=ALL&fullname=true'.format(start_date)
+    raw_data = requests.get(url).text
+    json_data = json.loads(raw_data)
+    asteroid_data = json_data['data']
+
+    asteroid_velocity_data = {}
+    ############################################################
+    # structure:
+    # {
+    #    name:{
+    #       velocity, body, designation
+    #    }
+    # }
+    # asteroid data includes:
+    #   name: asteroid full name
+    #   designation: use to recognize which asteroid it is, often same as name
+    #   velocity: velocity relative to the approach body at close approach, in km/s
+    #   body: name of the close-approach body, e.g., Earth
+    ############################################################
+    for asteroid in asteroid_data:
+        asteroid_data[asteroid[11].strip()] = {
+            'velocity': asteroid[7],
+            'body': asteroid[10],
+            'designation': asteroid[0]
+        }
+
+    return asteroid_velocity_data
+
+
+def store_neo_in_db(dic, db, sheet_name):
     ###############################
     # receive 2 arguments:
-    #   dic: the dictionary you want to store into database
+    #   dic: the neo dictionary you want to store into database
     #       dictionary key should be the unique id of each row
     #   db: database file path
+    #   sheet_name: name of table in which data stored
     ###############################
     conn = sqlite3.connect(db)
     cur = conn.cursor()
 
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS {}(
+        estimated_min ,
+        estimate_max,
+        is_potentially_hazardous
+    )
+    '''.format(sheet_name))
+
+
+
 
 if __name__ == '__main__':
-    print(os.getcwd())
+    conn = sqlite3.connect(DB)
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS test(
+        name TEXT,
+        diameter INT
+    )
+    ''')
