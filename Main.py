d8fa8403306d05e6766948927694be2679d73131
zdongyu <zdongyu@umich.edu>
2019-12-19 20:23:03 -0500

    sentry calc added to main

--- Main.py
+++ Main.py
@@ -12,37 +12,30 @@ start_date_1 = datetime.date(2018, 5, 1)
 start_date_2 = datetime.date(2018, 10, 1)
 
 # NEO data
-for i in range(10):
-    date = start_date_1 + datetime.timedelta(days=i)
-    store_neo_in_db(get_neo_data(date, NEO_API_KEY), DB, 'NEO_2018_05')
-
-for i in range(10):
-    date = start_date_2 + datetime.timedelta(days=i)
-    store_neo_in_db(get_neo_data(date, NEO_API_KEY), DB, 'NEO_2018_10')
-
-# Sentry data
-for i in range(7):
-    store_sentry_in_db(get_sentry_data(6), DB, 'larger_than_1e6')
-
-for i in range(7):
-    store_sentry_in_db(get_sentry_data(5), DB, 'larger_than_1e5')
-
-# Close-Approach data
-for i in range(8):
-    date = start_date_1 + datetime.timedelta(weeks=i)
-    store_cad_in_db(get_ca_data(date), DB, 'CAD_2018_05')
-
-for i in range(8):
-    date = start_date_2 + datetime.timedelta(weeks=i)
-    store_cad_in_db(get_ca_data(date), DB, 'CAD_2018_10')
+# for i in range(10):
+#     date = start_date_1 + datetime.timedelta(days=i)
+#     store_neo_in_db(get_neo_data(date, NEO_API_KEY), DB, 'NEO_2018_05')
+#
+# for i in range(10):
+#     date = start_date_2 + datetime.timedelta(days=i)
+#     store_neo_in_db(get_neo_data(date, NEO_API_KEY), DB, 'NEO_2018_10')
+#
+# # Sentry data
+# for i in range(7):
+#     store_sentry_in_db(get_sentry_data(6), DB, 'larger_than_1e6')
+#
+# for i in range(7):
+#     store_sentry_in_db(get_sentry_data(5), DB, 'larger_than_1e5')
+#
+# # Close-Approach data
+# for i in range(8):
+#     date = start_date_1 + datetime.timedelta(weeks=i)
+#     store_cad_in_db(get_ca_data(date), DB, 'CAD_2018_05')
+#
+# for i in range(8):
+#     date = start_date_2 + datetime.timedelta(weeks=i)
+#     store_cad_in_db(get_ca_data(date), DB, 'CAD_2018_10')
 
 
 # calculation and plotting
-def averageVelocity(body):
-    count = 0
-    total = 0
-    for x in body:
-        total += x[0]
-        count += 1
-    return total/count
-
+calculate_volume_sentry(DB)
